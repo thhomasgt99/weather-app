14a35c9f24178eba32b93c29644fb1c891ca96a7
Thomas Bernal <thhomasgt99@gmail.com>
2022-08-04 11:23:31 -0500

    github pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/Footer.css';
 const Footer = () => {
     return (
         <footer>
-            <p>Created by <a href="https://github.com/ivantdev/">ivantdev</a></p>
+            <p>Created by <a href="https://github.com/thhomagt99/">Thomas</a></p>
             <p></p>
         </footer>
     )
